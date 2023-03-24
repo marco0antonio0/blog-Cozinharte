ce97c96e56d87d6ae411174f8f588c6a0ab4c28d
marco0antonio0 <marcomesquitajr@hotmail.com>
2023-03-24 20:01:48 -0300

    up

---
+++ android/app/src/main/kotlin/com/example/blog_grupo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blog_grupo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page_1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PaginaInicio extends StatefulWidget {
+  const PaginaInicio({super.key});
+
+  @override
+  State<PaginaInicio> createState() => _PaginaInicioState();
+}
+
+class _PaginaInicioState extends State<PaginaInicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: []),
+    );
+  }
+}
