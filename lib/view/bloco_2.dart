a9b824154c90db5a9a0cb3752c0816a17cef3cc2
kauanleandr <kauanprograma@gmail.com>
2023-03-24 22:53:24 -0300

    alterei o bloco_2 da pagina

--- lib/view/bloco_2.dart
+++ lib/view/bloco_2.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 class bloco_2 extends StatefulWidget {
@@ -10,8 +12,40 @@ class bloco_2 extends StatefulWidget {
 class _bloco_2State extends State<bloco_2> {
   @override
   Widget build(BuildContext context) {
+    double largura = MediaQuery.of(context).size.width;
+    double responsive = largura > 1100
+        ? 120
+        : largura > 750
+            ? 90
+            : 80;
     return Column(
-      children: [],
+      children: [
+        bloco_0(),
+        Container(
+          height: 30,
+        ),
+        bloco_1(responsive)
+      ],
     );
   }
+}
+
+Widget bloco_0() {
+  return Container(
+    decoration: BoxDecoration(
+        color: Color(0xffF8B182), borderRadius: BorderRadius.circular(50)),
+    margin: EdgeInsets.symmetric(horizontal: 100),
+    height: 500,
+    alignment: Alignment.bottomCenter,
+    padding: EdgeInsets.only(left: 70),
+  );
+}
+
+Widget bloco_1(responsive) {
+  return Container(
+    height: responsive,
+    alignment: Alignment.bottomCenter,
+    padding: EdgeInsets.only(left: 70),
+    color: Color(0xffF67968),
+  );
 }
