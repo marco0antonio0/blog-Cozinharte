7a0d5e0965382fffb1198c10891bcbe02a558fe4
kauanleandr <kauanprograma@gmail.com>
2023-03-29 22:07:40 -0300

    up

--- lib/view/bloco_2.dart
+++ lib/view/bloco_2.dart
@@ -3,7 +3,7 @@ import 'dart:js';
 import 'package:blog_grupo/view/widget.dart';
 import 'package:flutter/material.dart';
 
-//=================================================================== essa é a minha
+//=================================================================== essa é a minha alteração
 //
 class bloco_2 extends StatefulWidget {
   const bloco_2({super.key});
